167b81216a6c04723df4e3413272d7ce389f31a9
Mikray Rahman <milareheman@gmail.com>
2019-09-16 17:43:12 -0400

    first commit

---
+++ git/Task03.java
@@ -0,0 +1,37 @@
+package Assignments;
+
+public class Task03 {
+	public static void main(String[] args) {
+		
+		/*
+		 Create a class CompanyInfo
+
+Declare variables:
+            companyName
+            address
+            numberOfEmployees
+            revenue
+            isTechCompany
+
+Print statements and describe each value:
+
+            Company name is Facebook
+            address is ...
+            Number of employees is 100
+            Annual revenue is 4 000 000
+            is it tech company? - true
+		 */
+				String companyName="Facebook";
+				String address="...";
+				int numberOfEmployees=100;
+				String revenue="4 000 000";
+				boolean techCompany=true;
+		
+		System.out.println("Company name"+" is "+companyName);
+		System.out.println("address is"+ address);
+		System.out.println("Number of employees is " + numberOfEmployees);
+		System.out.println("Annual revenue is "+ revenue);
+		System.out.println("is it tech company?"+ " - "+ techCompany);
+	}
+
+}
